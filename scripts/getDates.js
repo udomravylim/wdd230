dc40ba84ac5dcff62152fef3a57135694ca7aa5e
Ravy Lim <lim21012@byui.edu>
2024-01-18 16:48:39 -0700

    1st try

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,15 @@
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
+// Create current year, name and country
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
 
-var copyrightParagraph = document.getElementById("copyright");
+// let copyrightParagraph = document.querySelector("#copyright");
+let copyrightParagraph = document.getElementById("copyright");
+
+/* we can also do: copyrightParagraph.innerHTML = `&copy;${currentYear} 💕 Ravy Lim 💕 Cambodia`
+Because &copy; is the HTML unity, so we use .innerHTMl instead of .textContent */
 copyrightParagraph.textContent += `${currentYear} 💕 Ravy Lim 💕 Cambodia`;
-var lastModifiedParagraph = document.getElementById("lastModified");
-lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
+
+//Create lastModified by using lastModified property in JS
+const lastModified = document.lastModified;
+const lastModifiedParagraph = document.getElementById("lastModified");
+lastModifiedParagraph.textContent = `Last Modified: ${lastModified}`;
