bc4cbf92d18a9c60c111a1a00f19ec83a5992cea
Ravy Lim <lim21012@byui.edu>
2024-02-28 21:17:55 -0700

    add join us.html page

---
+++ chamber/scripts/timestamp.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    // Get the current date and time
+    var currentDateTime = new Date().toISOString();
+    
+    // Set the value of the hidden input field to the current date and time
+    document.getElementById("timestamp").value = currentDateTime;};
